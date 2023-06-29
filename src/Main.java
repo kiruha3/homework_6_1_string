f7844c266e4996a8a845096c32d42f7e15847c8a
kirill <kiruha3@mail.ru>
2023-06-29 10:03:43 +0700

    1 задача

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        task1();
+        task2();
+        task3();
+    }
+
+    private static void task1() {
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+        String fullName = lastName + " " + firstName + " " + middleName;
+        System.out.println("ФИО сотрудника - " + fullName);
+    }
+
+    private static void task2() {
+    }
+
+    private static void task3() {
     }
 }
