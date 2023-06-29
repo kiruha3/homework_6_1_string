845a24920747e5d94eb5a5baacec3fc7db859ce9
kirill <kiruha3@mail.ru>
2023-06-29 10:13:08 +0700

    1,2,3 задача

--- src/Main.java
+++ src/Main.java
@@ -14,8 +14,20 @@ public class Main {
     }
 
     private static void task2() {
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+        String fullName = lastName + " " + firstName + " " + middleName;
+        String fullNameToUpperCase = fullName.toUpperCase();
+        System.out.println("Данные ФИО сотрудника для заполнения отчета — " + fullNameToUpperCase);
     }
 
     private static void task3() {
+        String firstName = "Семён";
+        String middleName = "Семёнович";
+        String lastName = "Иванов";
+        String fullName = lastName + " " + firstName + " " + middleName;
+        fullName = fullName.replace("ё", "e");
+        System.out.println("Данные ФИО сотрудника — " + fullName);
     }
 }
